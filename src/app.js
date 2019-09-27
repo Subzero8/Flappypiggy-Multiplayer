1f5028183da71ed97861c31bad3c0ebf840ec9ac
Subzero8 <alexandredepape@gmail.com>
2019-09-28 00:31:28 +0200

    yup

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const Match = require('./js/server/Match.js');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 let sockets = [];
 let availableSockets = [];
